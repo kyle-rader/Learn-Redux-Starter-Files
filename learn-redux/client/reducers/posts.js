0d70b5815f955ff50291a95e10ed5c5a69c336c0
Kyle Rader <kyle@kylerader.ninja>
2017-03-11 22:30:34 -0800

    implement inc likes in posts reducer

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -2,6 +2,16 @@
 // and returns: the new state.
 
 export default function posts(state = [], action) {
-  console.log(state, action);
-  return state;
+  switch(action.type) {
+    case 'INCREMENT_LIKES':
+      // return updated state
+      const i = action.index;
+      return [
+        ...state.slice(0,i), // before our index
+        {...state[i], likes: state[i].likes + 1}, // update out post in question
+        ...state.slice(i + 1), // everything after our index
+      ];
+    default:
+      return state;
+  }
 }
