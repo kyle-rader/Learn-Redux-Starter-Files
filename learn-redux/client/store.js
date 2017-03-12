50456e9d0fb1e5f3695441e048715f1af6a71f4e
Kyle Rader <kyle@kylerader.ninja>
2017-03-11 21:32:19 -0800

    add store and action creators

---
+++ learn-redux/client/store.js
@@ -0,0 +1,19 @@
+import { createStore compose } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+// import our root reducer
+import rootReducer from './reducers/index';
+
+// import our dummy data
+import posts from './data/posts';
+import comments from './data/comments';
+
+// create an object for the default data
+const defaultState = {
+  posts,
+  coments,
+};
+
+export default const store = createStore(rootReducer, defaultState);
+export const history = syncHistoryWithStore(browserHistory, store);
