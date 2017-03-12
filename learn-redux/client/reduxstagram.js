9a4a29af766c8c493215e34db5546f70a528e3cb
Kyle Rader <kyle@kylerader.ninja>
2017-03-11 21:09:49 -0800

    Add initial Main component

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,10 @@
-// let's go!
+import React, { Component, PropTypes } from 'react';
+import { render } from 'react-dom';
+
+// Import some CSS
+import css from './styles/style.styl'
+
+// Import Components
+import Main from './components/Main';
+
+render(<Main/>, document.getElementById('root'));
